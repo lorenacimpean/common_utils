94c14d2cd3763198287930893eaf297777efccd3
Lorena <lorena@Lorenas-MacBook-Pro.local>
2025-11-30 12:10:34 +0200

    added example

--- lib/common_utils.dart
+++ lib/common_utils.dart
@@ -1,11 +1,8 @@
-library;
-
+export 'src/theming/font_sizes.dart';
 export 'src/theming/soft_grid.dart';
 export 'src/theming/spacer.dart';
-export 'src/theming/typography.dart';
+export 'src/utils/collection_extensions.dart';
 export 'src/utils/context_shortcuts.dart';
 export 'src/utils/debug_logger.dart';
 export 'src/utils/platform_utils.dart';
-export 'src/utils/validation/common_string_extensions.dart';
 export 'src/utils/validation/form_validator.dart';
-export 'src/utils/validation/validation_extension.dart';
